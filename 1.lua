23fe21a55dd9da2b138a4882ee3a1100d0cf10dc
globalbankingelites <126375907+globalbankingelites@users.noreply.github.com>
2023-03-19 15:40:15 +0100

    Create 1.lua

---
+++ 1.lua
@@ -0,0 +1,8 @@
+local key = loadstring(game:HttpGet("https://raw.githubusercontent.com/HoyoGey/My-Lua-Project/main/UiLibs/KeySysUiLib.lua"))()
+key:MakeUI({
+  Title = "ThugHub", --// dont need to explain
+  KeyLinkName = "ThugHub", --// the link u chose thru the bot.
+  DiscordLink = "discord.gg/y6ScykvEg9", --// replace the numbers by your discord invite
+  ScriptLink = "https://raw.githubusercontent.com/globalbankingelites/Scripts/main/fake.lua" --// this is where u put the raw link to ur source
+  FileKey = "ThugHub.lua" --// you will need to match this in the source version.
+})
